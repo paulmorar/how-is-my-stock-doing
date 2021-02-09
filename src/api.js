e670ceccb440b4e45aa0f372628ac78b0b636fb0
Paul Morar <paulcornelmorar@gmail.com>
2021-02-09 15:30:54 +0100

    Allow all cors

--- src/api.js
+++ src/api.js
@@ -3,23 +3,8 @@ const cors = require('cors');
 const serverless = require("serverless-http");
 const yahooFinance = require("yahoo-finance");
 
-var allowedOrigins = ['http://localhost:3000',
-                      'https://www.howisgmedoing.com/'];
-
 const app = express();
-app.use(cors({
-  origin: (origin, callback) => {
-    // allow requests with no origin 
-    // (like mobile apps or curl requests)
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors());
 
 const router = express.Router();
 
