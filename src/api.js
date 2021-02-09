225b73e004879f71ce9bf900d0c246a0d59351eb
Paul Morar <paulcornelmorar@gmail.com>
2021-02-09 14:40:46 +0100

    Add cors configuration

--- src/api.js
+++ src/api.js
@@ -1,8 +1,27 @@
 const express = require("express");
+const cors = require('cors');
 const serverless = require("serverless-http");
 const yahooFinance = require("yahoo-finance");
 
+var allowedOrigins = ['http://localhost:3000',
+                      'http://howisgmedoing.com',
+                      'https://howisgmedoing.com'];
+
 const app = express();
+app.use(cors({
+  origin: (origin, callback) => {
+    // allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
+
 const router = express.Router();
 
 router.get("/", (req, res) => {
